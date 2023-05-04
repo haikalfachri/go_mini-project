ec4a5d5854fc06f410008b2ee3d8d5e7f08b2937
Muhamad Fachri Haikal <haikal.fachri18@gmail.com>
2023-05-04 21:58:35 +0700

    chore(app):remove unused api in controllers

--- controllers/order.go
+++ controllers/order.go
@@ -49,24 +49,6 @@ func (uc *OrderController) Create(c echo.Context) error {
 	})
 }
 
-func (uc *OrderController) GetByName(c echo.Context) error {
-	orders, err := uc.service.GetAll()
-
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, response.Response[any]{
-			Status 	: "failed",
-			Message	: "failed to fetch a order by name",
-			Error	:  err.Error(),
-		})
-	}
-
-	return c.JSON(http.StatusOK, response.Response[any]{
-		Status 	: "success",
-		Message	: "success to fetch a order by name",
-		Data	:  orders,
-	})
-}
-
 func (uc *OrderController) GetAll(c echo.Context) error {
 	orders, err := uc.service.GetAll()
 
@@ -82,25 +64,6 @@ func (uc *OrderController) GetAll(c echo.Context) error {
 		Status 	: "success",
 		Message	: "success to fetch all order",
 		Data	:  orders,
-	})
-}
-
-func (uc *OrderController) GetById(c echo.Context) error {
-	id := c.Param("id")
-
-	order, err := uc.service.GetById(id)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, response.Response[any]{
-			Status 	: "failed",
-			Message	: "failed to fetch a order by id",
-			Error	:  err.Error(),
-		})
-	}
-
-	return c.JSON(http.StatusOK, response.Response[any]{
-		Status 	: "success",
-		Message	: "success to fetch a order by id",
-		Data	:  order,
 	})
 }
 
@@ -122,53 +85,6 @@ func (uc *OrderController) GiveRating(c echo.Context) error {
 		Status 	: "success",
 		Message	: "success to give rating of an order",
 		Data	:  order,
-	})
-}
-
-func (uc *OrderController) Update(c echo.Context) error {
-	var orderInput input.OrderInput
-	c.Bind(&orderInput)
-
-	err := orderInput.Validate()
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, response.Response[any]{
-			Status 	: "failed",
-			Message	: "input invalid",
-			Error	:  err.Error(),
-		})
-	}
-
-	id := c.Param("id")
-	order, err := uc.service.Update(orderInput, id)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, response.Response[any]{
-			Status 	: "failed",
-			Message	: "failed to update a order",
-			Error	:  err.Error(),
-		})
-	}
-	
-	return c.JSON(http.StatusOK, response.Response[any]{
-		Status 	: "success",
-		Message	: "success to update a order",
-		Data	:  order,
-	})
-}
-
-func (uc *OrderController) Delete(c echo.Context) error {
-	id := c.Param("id")
-
-	if err := uc.service.Delete(id); err != nil {
-		return c.JSON(http.StatusBadRequest, response.Response[any]{
-			Status 	: "failed",
-			Message	: "failed to delete a order",
-			Error	:  err.Error(),
-		})
-	}
-
-	return c.JSON(http.StatusOK, response.Response[any]{
-		Status 	: "success",
-		Message	: "success to delete a order",
 	})
 }
 
